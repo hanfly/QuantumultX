5286abcb91a66604de426f81651a6c6267f0a124
Pengfei Han <h23006@gmail.com>
2022-03-28 22:38:13 +0800

    Update resource-parser.js

--- Scripts/resource-parser.js
+++ Scripts/resource-parser.js
@@ -1994,7 +1994,7 @@ function get_emoji(emojip, sname) {
   }
     str1 = JSON.stringify(Lmoji)
     aa = JSON.parse(str1)
-    bb = JSON.parse(str1.replace(/🇹🇼/g, "🏳️‍🌈"))
+    bb = JSON.parse(str1.replace(/🇹🇼/g, "🇳🇫"))
     var cnt = emojip ==1? aa:bb;
     var flag = 0;
     for (var key in cnt) {
